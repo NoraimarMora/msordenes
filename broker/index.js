1ce1e9853d44519b1732ec86440c3414a5a7d510
Angel Rodríguez <angelmanuelrs@gmail.com>
2019-10-18 11:12:32 -0400

    [NOTICKET] - Add message broker integration to notify allies

---
+++ broker/index.js
@@ -0,0 +1,33 @@
+const amqplib = require('amqplib');
+
+const { BROKER_URL } = require('../config');
+
+let brokerConnection = null;
+
+const initBroker = () => new Promise(async (resolve, reject) => {
+  try {
+    const connection = await amqplib.connect(url);
+    resolve(connection);
+  } catch (error) {
+    console.warning(error);
+    reject(error);
+  }
+});
+
+const notifyOrderCreated = async (queue, order) => {
+  if (!brokerConnection) {
+    brokerConnection = await initBroker(BROKER_URL);
+  }
+
+  await brokerConnection.createChannel()
+    .then(ch => ch.assertQueue(queue)
+      .then(() => {
+        const orderObj = JSON.stringify(order);
+        ch.sendToQueue(queue, Buffer.from(orderObj));
+      })
+    ).catch(console.warning);
+}
+
+module.exports = {
+  notifyOrderCreated,
+};
--- controllers/orden.js
+++ controllers/orden.js
@@ -3,6 +3,7 @@
 var Orden = require('../models/orden');
 var ElementoOrden = require('../models/elementoOrden');
 var mongoose = require('mongoose');
+const { notifyOrderCreated } = require('../broker');
 
 var controller = {
 
@@ -49,6 +50,24 @@ var controller = {
                 if (!ordenStored) {
                     return response.status(404).send({message: 'No se ha podido guardar el documento'});
                 }
+
+                /*
+                  TODO: adapt `ordenNotifyObj` structure to be:
+
+                  ```
+                    orderId,
+                    storeId: '1234',
+                    assignedCourierName: faker.name.findName(),
+                    products: [
+                      { name: 'Hamburguesa de queso', qty: faker.random.number(), img: 'https://img1.mashed.com/img/gallery/fast-food-hamburgers-ranked-worst-to-best/intro-1540401194.jpg' },
+                      { name: 'Refresco', qty: faker.random.number(), img: 'https://secure.ce-tescoassets.com/assets/CZ/202/8594008040202/ShotType1_540x540.jpg' },
+                    ]
+                  ```
+                */
+
+                // const ordenNotifyObj = {};
+                // notifyOrderCreated('task1', ordenNotifyObj);
+
                 return response.status(200).send({orden: ordenStored});
             });
         } catch (error) {
